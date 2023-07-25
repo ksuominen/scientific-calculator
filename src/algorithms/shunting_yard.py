a869b3882b368da55492b92a367998b3d16fc817
Kristiina Suominen <13131792+ksuominen@users.noreply.github.com>
2023-07-25 15:35:31 +0300

    Create first version of shunting yard + tests, works with basic arithmetic expressions

---
+++ src/algorithms/shunting_yard.py
@@ -0,0 +1,71 @@
+operator_precedences = {"+": 2, "-": 2, "*": 3, "/": 3, "^": 4}
+
+operator_associativities = {
+    "+": "left",
+    "-": "left",
+    "*": "left",
+    "/": "left",
+    "^": "right",
+}
+
+
+def get_precedence(operator):
+    return operator_precedences.get(operator)
+
+
+def get_associativity(operator):
+    return operator_associativities.get(operator)
+
+
+def is_left_associative(operator):
+    return get_associativity(operator) == "left"
+
+
+def is_operator(token):
+    return token in operator_precedences.keys()
+
+
+def is_number(token):
+    return isinstance(token, (int, float))
+
+
+def shunting_yard(input):
+    output = []
+
+    stack = []
+
+    for token in input:
+        if is_number(token):
+            output.append(token)
+
+        elif is_operator(token):
+            if len(stack) == 0 or stack[-1] == "(":
+                stack.append(token)
+                continue
+            prec = get_precedence(token)
+            if prec > get_precedence(stack[-1]) or (
+                prec > get_precedence(stack[-1]) and not is_left_associative(token)
+            ):
+                stack.append(token)
+                continue
+
+            while (stack and stack[-1] != "(") and (
+                prec < get_precedence(stack[-1])
+                or (prec == get_precedence(stack[-1]) and is_left_associative(token))
+            ):
+                output.append(stack.pop())
+            stack.append(token)
+
+        elif token == "(":
+            stack.append(token)
+
+        elif token == ")":
+            stack_top = stack.pop()
+            while stack_top != "(":
+                output.append(stack_top)
+                stack_top = stack.pop()
+
+    while stack:
+        output.append(stack.pop())
+
+    return output
---
+++ src/tests/shunting_yard_test.py
@@ -0,0 +1,87 @@
+import unittest
+from algorithms import shunting_yard as sy
+
+
+class TestShuntingYard(unittest.TestCase):
+    def test_get_precedence_plus(self):
+        precedence = sy.get_precedence("+")
+        self.assertEqual(precedence, 2)
+
+    def test_get_precedence_power(self):
+        precedence = sy.get_precedence("^")
+        self.assertEqual(precedence, 4)
+
+    def test_get_associativity_division(self):
+        assoc = sy.get_associativity("/")
+        self.assertEqual(assoc, "left")
+
+    def test_get_associativity_power(self):
+        assoc = sy.get_associativity("^")
+        self.assertEqual(assoc, "right")
+
+    def test_multiplication_is_left_associative(self):
+        self.assertTrue(sy.is_left_associative("*"))
+
+    def test_power_is_not_left_associative(self):
+        self.assertFalse(sy.is_left_associative("^"))
+
+    def test_is_operator_minus(self):
+        self.assertTrue(sy.is_operator("-"))
+
+    def test_char_not_operator(self):
+        self.assertFalse(sy.is_operator("b"))
+
+    def test_number_not_operator(self):
+        self.assertFalse(sy.is_operator(5))
+
+    def test_is_number_int(self):
+        self.assertTrue(sy.is_number(7))
+
+    def test_is_number_float(self):
+        self.assertTrue(sy.is_number(17.34))
+
+    def test_string_is_not_number(self):
+        self.assertFalse(sy.is_number("Hello"))
+
+    def test_operator_is_not_number(self):
+        self.assertFalse(sy.is_number("^"))
+
+    def test_shunting_yard_1(self):
+        operation = [4, "+", 18, "/", "(", 9, "-", 3, ")"]
+        result = [4, 18, 9, 3, "-", "/", "+"]
+        self.assertEqual(sy.shunting_yard(operation), result)
+
+    def test_shunting_yard_2(self):
+        operation = [3, "+", 4, "*", 2, "/", "(", 1, "-", 5, ")", "^", 2, "^", 3]
+        result = [3, 4, 2, "*", 1, 5, "-", 2, 3, "^", "^", "/", "+"]
+        self.assertEqual(sy.shunting_yard(operation), result)
+
+    def test_shunting_yard_3(self):
+        operation = [1, "*", 2, "+", 3]
+        result = [1, 2, "*", 3, "+"]
+        self.assertEqual(sy.shunting_yard(operation), result)
+
+    def test_shunting_yard_4(self):
+        operation = [1, "+", 2, "*", 3]
+        result = [1, 2, 3, "*", "+"]
+        self.assertEqual(sy.shunting_yard(operation), result)
+
+    def test_shunting_yard_5(self):
+        operation = [1, "*", "(", 2, "+", 3, ")"]
+        result = [1, 2, 3, "+", "*"]
+        self.assertEqual(sy.shunting_yard(operation), result)
+
+    def test_shunting_yard_6(self):
+        operation = [1, "-", 2, "+", 3]
+        result = [1, 2, "-", 3, "+"]
+        self.assertEqual(sy.shunting_yard(operation), result)
+
+    def test_shunting_yard_7(self):
+        operation = [1, "*", 2, "^", 3, "+", 4]
+        result = [1, 2, 3, "^", "*", 4, "+"]
+        self.assertEqual(sy.shunting_yard(operation), result)
+
+    def test_shunting_yard_8(self):
+        operation = [1, "*", "(", 2, "+", 3, "*", 4, ")", "+", 5]
+        result = [1, 2, 3, 4, "*", "+", "*", 5, "+"]
+        self.assertEqual(sy.shunting_yard(operation), result)
